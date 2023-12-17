267b52128174f52f50a7c3ac34b69df87a32b4a9
raphhawk <bing619.at@gmail.com>
2023-12-17 18:09:27 +0530

    adding mailer

--- cmd/web/mailer.go
+++ cmd/web/mailer.go
@@ -1,1 +1,17 @@
 package main
+
+import "sync"
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	Wait        *sync.WaitGroup
+	MailerChan  chan Message
+	ErrorChan   chan error
+	DoneChan    chan bool
+}
